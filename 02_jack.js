8352e8ed511540ebd0d1955981a1e3ad1a7cf348
Jack Edgar <jackedgar@seattleacademy.org>
2016-02-09 14:56:44 -0800

    add 02_jack.js

--- 02_jack.js
+++ 02_jack.js
@@ -2,3 +2,19 @@ console.log(process.argv);
 var pin = process.argv[2];
 var status = process.argv[3];
 console.log("pin number " + pin + " turn " + status)
+var wpi = require("wiring-pi");
+
+//console.log("wpi",wpi);
+wpi.setup("gpio");
+
+wpi.pinMode(pin*1,wpi.OUTPUT);
+
+var value = 1;
+
+function toggleBulb(){
+	wpi.digitalWrite(pin*1,value);
+	console.log("pin",pin,"value",value)
+	value = +!value;
+}
+toggleBulb();
+setInterval(toggleBulb,500);
